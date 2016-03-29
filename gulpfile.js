8bb20009918f364cf7009fb0bda8fc1ecd131985
lmeijdam <lmeijdam90@gmail.com>
2016-03-29 08:29:13 +0200

    updated gulpfile structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,35 @@
-var gulp = require('gulp');
-var del = require('del');
-var sourcemaps = require('gulp-sourcemaps');
-var jsuglify = require('gulp-uglify');
-var typescript = require('gulp-typescript');
-var tslint = require('gulp-tslint');
-var tsProject = typescript.createProject('tsconfig.json');
-var concatCss = require('gulp-concat-css');
+var gulp = require('gulp'),
+    del = require('del'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jsuglify = require('gulp-uglify'),
+    typescript = require('gulp-typescript'),
+    tslint = require('gulp-tslint'),
+    tsProject = typescript.createProject('tsconfig.json'),
+    concatCss = require('gulp-concat-css');
 
-const tscConfig = require('./tsconfig.json');
+var appDev = 'dev/',
+    appProd = 'app/',
+    appView = 'view/',
+    appStyles = 'style/'
 
-//var cssProd = 'css/';
-var appDev = 'dev/';
-var appProd = 'app/';
-var appView = 'view/';
-var appStyles = 'style/';
-
-gulp.task('tslint', function () {
+// Tasks
+gulp.task('tslint', function() {
     return gulp.src(appDev + '**/*.ts')
         .pipe(tslint())
         .pipe(tslint.report("verbose"));
 });
 
-gulp.task('clean-app', function () {
+gulp.task('clean-app', function() {
     return del(appProd + '**/*');
 });
 
-gulp.task('clean-view', function () {
+gulp.task('clean-view', function() {
     return del(appView + '**/*');
 });
 
 
 // TypeScript compile
-gulp.task('build-ts', ['clean-app', 'tslint'], function () {
+gulp.task('build-ts', ['clean-app', 'tslint'], function() {
     return gulp
         .src(appDev + '**/*.ts')
         .pipe(sourcemaps.init())          // <--- sourcemaps
@@ -41,18 +39,17 @@ gulp.task('build-ts', ['clean-app', 'tslint'], function () {
 });
 
 // HTML and CSS
-gulp.task('build-html', ['clean-view'], function () {
+gulp.task('build-html', ['clean-view'], function() {
     return gulp.src(appDev + '**/*.html')
         .pipe(gulp.dest(appView));
 });
-
-gulp.task('build-css', function () {
+gulp.task('build-css', function() {
     return gulp.src(appDev + '**/*.css')
         .pipe(concatCss('style.min.css'))
         .pipe(gulp.dest(appStyles));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch(appDev + '**/*.ts', ['build-ts']);
     gulp.watch(appDev + '**/*.html', ['build-html']);
     gulp.watch(appDev + '**/*.css', ['build-css']);
